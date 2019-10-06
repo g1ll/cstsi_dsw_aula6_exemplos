a7981ed8d61c75ea24ba842a4e6dfca26bc26955
Gill <gillvelleda@gmail.com>
2019-10-05 21:44:21 -0300

    criando propriedade url

--- src/apiProdFetch.js
+++ src/apiProdFetch.js
@@ -3,9 +3,11 @@ import axios from 'axios';
  //function ApiProdFetch() {//função para pegar os dados da API
  const ApiProdFetch = {
 
+  url: 'http://localhost/2019/tsi/dsw/apiProds/produto/',
+  
   searchProd: async function(termo){
     console.log(termo);
-    const resp = await axios.get('http://localhost/2019/tsi/dsw/apiProds/produto/all/'+termo)
+    const resp = await axios.get(`${this.url}all/${termo}`)
     console.log({'data':resp.data})
     if(resp.data.length>0)
       return resp.data
@@ -15,7 +17,7 @@ import axios from 'axios';
 
   getProdById: async function(id){
     console.log(id);
-    const resp = await axios.get('http://localhost/2019/tsi/dsw/apiProds/produto/'+id)
+    const resp = await axios.get(this.url+id)
     if(resp.data.length>0)
     return resp.data
   else
